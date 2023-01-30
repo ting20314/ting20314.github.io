756f3ecca22148df45af81016cb9c793340216a6
ting20314 <67890382+ting20314@users.noreply.github.com>
2023-01-30 17:20:18 +0100

    Update source_code.js

--- js/source_code.js
+++ js/source_code.js
@@ -47,17 +47,7 @@ closeModalButtons.forEach(button => {
     })
 })
 
-function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
-}
 
-function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
-}
 	
 	
 	
